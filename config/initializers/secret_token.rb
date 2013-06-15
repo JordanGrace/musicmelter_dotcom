d48c8fc1417765295e42fc08ecbdedf4e44af691
Charles Butler <chuck@dasroot.net>
2013-06-15 02:03:50 -0400

    Removes the session initializer

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Musicmelter::Application.config.secret_token = '52b988633b9cfba772ff9e95e3c060c7fe442255792e1d574642548ca24307575880117665fa256b9d865c8ebd9b6df3c35e766d3f48d8f8dc570cfa81435c36'
