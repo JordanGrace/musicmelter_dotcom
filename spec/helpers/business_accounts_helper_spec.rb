19eae758ef87645bbeacf486a855b2484cc68abe
Charles Butler <chuck@dasroot.net>
2013-06-15 03:17:10 -0400

    Adds test specs

---
+++ spec/helpers/business_accounts_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the BusinessAccountsHelper. For example:
+#
+# describe BusinessAccountsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe BusinessAccountsHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
