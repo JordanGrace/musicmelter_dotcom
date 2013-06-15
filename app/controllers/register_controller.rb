e002edda699d6f1ba88fda96e04b0a276652621c
Charles Butler <chuck@dasroot.net>
2013-06-15 14:19:00 -0400

    Adds stray helpers, and business registration path iteration 1

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,5 +1,13 @@
 class RegisterController < ApplicationController
   def business
+    @business_types = [
+                        ['1','Store'],
+                        ['2','Tutor'],
+                        ['3','WholeSaler'],
+                        ['4','Venue'],
+                        ['5','Studio'],
+                        ['6','Producer']
+                    ]
   end
 
   def user
