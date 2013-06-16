fe109131162ecb788946b424c09a96e9a05f3f0d
Charles Butler <chuck@dasroot.net>
2013-06-15 23:39:23 -0400

    Adds mailtrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,15 @@ Musicmelter::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+    if ENV['MAILTRAP_HOST'].present?
+      ActionMailer::Base.delivery_method = :smtp
+      ActionMailer::Base.smtp_settings = {
+        :user_name => ENV['MAILTRAP_USER_NAME'],
+        :password => ENV['MAILTRAP_PASSWORD'],
+        :address => ENV['MAILTRAP_HOST'],
+        :port => ENV['MAILTRAP_PORT'],
+        :authentication => :plain
+      }
+    end
 end
