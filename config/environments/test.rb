be68e6df5da271fe861947bc6ed28b2367b0eed9
Charles Butler <chuck@dasroot.net>
2013-06-21 22:35:55 -0400

    Added sandbox API key to test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Musicmelter::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  ENV['stripe_key'] = 'sk_test_WQ62JCppYxctpsEiH0GBvfN6'
 end
