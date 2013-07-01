11417373e95a0601ddabaa13df7ad8715b2c0218
Charles Butler <chuck@dasroot.net>
2013-06-30 22:02:52 -0400

    Adds a predicate value and helper to determine if the site is accepting stripe or paypal payment methods

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -1,2 +1,11 @@
 module SignupHelper
+	
+	def paypal_enabled?
+		SiteConfig.last.paypal_enabled
+	end
+
+	def stripe_enabled?
+		SiteConfig.last.stripe_enabled
+	end
+
 end
--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -1,17 +1,14 @@
 class SiteConfig
   include Mongoid::Document
-  field :default_subscription, type: String
-  field :default_price, type: String
-  field :accept_promos, type: Boolean
+  field :accept_promos,         type: Boolean
+  field :default_us_price,      type: BigDecimal
+  field :default_ca_price,      type: BigDecimal
+  field :stripe_enabled,        type: Boolean
+  field :paypal_enabled,        type: Boolean
 
-  validates_presence_of :accept_promos, :default_price, :default_subscription
-
-  before_save :process_money
+  validates_presence_of :accept_promos, :default_us_price, :default_ca_price
 
   private
 
-  def process_money
-    #default_price = number_to_currency(default_price, :unit => "$") 
-  end
 
 end
