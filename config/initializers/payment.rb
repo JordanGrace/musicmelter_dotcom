b65ed515ff2fb38cf89cdc75e06e3ebb0fa28c74
Charles Butler <chuck@dasroot.net>
2013-07-07 04:38:10 -0400

    Added paypal initializer values

---
+++ config/initializers/payment.rb
@@ -0,0 +1,18 @@
+require 'paypal/express'
+
+Rails.configuration.stripe = {
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+Rails.configuration.paypal = {
+	:client => ENV['PAYPAL_CLIENT'],
+	:secret => ENV['PAYPAL_SECRET'],
+	:user => ENV['PAYPAL_USER'],
+	:pass => ENV['PAYPAL_PASS'],
+	:signature => ENV['PAYPAL_SIGNATURE']
+}
+
+
