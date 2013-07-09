7e677fb0cb631f50c6f7c373a374d1954e5e8930
Charles Butler <chuck@dasroot.net>
2013-07-08 21:54:10 -0400

    typo

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -39,7 +39,7 @@ class Payment
     end  
     if self.paypal_token.present?
 	self.provider = "paypal"
-      	self.paypal_setup!("https://staging.musicmelter.com/payments/#{self.id}/success", "https://staging.musicmelter.com/payments/#{self.id}/cancel")
+      	self.paypal_setup!("https://staging.musicmelter.com/payment/#{self.id}/success", "https://staging.musicmelter.com/payment/#{self.id}/cancel")
     end
 end
 
