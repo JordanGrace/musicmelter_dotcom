68a93c91854c8b5b64a5d48d423bc14a5adfdd18
Charles Butler <chuck@dasroot.net>
2013-07-20 15:42:13 -0400

    Flag tweaks on biz reg, changes grill tile

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,8 @@
 
 $(document).ready(function() {
 	$('input[type=checkbox]').click(function() {
-		console.log($(this))
-		if ($(this).val() === 'Other' && $(this).attr('checked')) {
-			$('#hidden-other').removeClass('hidden')
-		} else {
-			$('#hidden-other').addClass('hidden')
+		if ($(this).val() === 'Other') {
+			$('#hidden-other').toggle();
 		}
 	})
 })
